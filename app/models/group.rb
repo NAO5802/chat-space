c8393efd7b275ebd6a6fa88d9b46a64de658048c
NAO5802 <i100185b@gmail.com>
2019-10-23 16:52:07 +0900

    define assosiation (user, group, message)

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :users_groups
   has_many :users, through: :users_groups
+  has_many :messages
 
   validates :name, presence: true
   validates :name, uniqueness: true
