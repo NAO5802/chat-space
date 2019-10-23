7103752d2b86442996c82396af5d6c5dc78319aa
NAO5802 <i100185b@gmail.com>
2019-10-23 15:43:40 +0900

    add "edit group" function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,10 +20,22 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    
   end
 
   def update
-    
+    if @group.update(group_params)
+      redirect_to groups_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
