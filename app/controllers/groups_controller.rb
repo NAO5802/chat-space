28e0da2cab7a74e90847d2e1871af842e7008423
NAO5802 <i100185b@gmail.com>
2019-10-23 15:42:31 +0900

    Add  "create new group" function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,22 @@
 class GroupsController < ApplicationController
-  def new
+  before_action :set_group, only: [:edit, :update]
+
+  def index
     
   end
 
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
   def create
-    
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   has_many :users_groups
   has_many :users, through: :users_groups
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
