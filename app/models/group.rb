44da34a5bdfb8e40ef0a53a1a6c40d31b49b08f7
NAO5802 <i100185b@gmail.com>
2019-10-25 11:45:48 +0900

    add show_last_message method

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,13 @@ class Group < ApplicationRecord
 
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
