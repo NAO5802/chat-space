db5a6b9d4a242b4151498d0428497bdbec99e6e1
NAO5802 <i100185b@gmail.com>
2019-10-25 11:45:03 +0900

    correct spell etc

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates :body, presence: true, if: :image.blank?
+  validates :body, presence: true, unless: :image?
 end
