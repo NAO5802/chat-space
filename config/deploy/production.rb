96d76a4fa7d8b369c3afe286845c9ac4c1611849
NAO5802 <i100185b@gmail.com>
2019-10-31 18:21:00 +0900

    delete logger

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,1 @@
 server '52.194.54.238', user: 'ec2-user', roles: %w{app db web}
-
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.logger    = ActiveSupport::TaggedLogging.new(logger)
-end
